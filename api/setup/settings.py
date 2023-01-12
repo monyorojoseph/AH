218ebbf480833212348a4a17d769bb6a397fb533
monyorojoseph <monyorojoseph@gmail.com>
2023-01-13 02:27:00 +0300

    staticfiles config

--- api/setup/settings.py
+++ api/setup/settings.py
@@ -144,6 +144,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "static"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+MEDDIA_URL = "media/"
+MEDDIA_ROOT = "media"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- api/setup/urls.py
+++ api/setup/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
@@ -13,3 +15,6 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
